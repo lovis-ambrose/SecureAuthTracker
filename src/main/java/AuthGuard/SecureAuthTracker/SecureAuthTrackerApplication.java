bda4d4874b040387b7d4e684969d7c9019fda4b1
lovis-ambrose <lovisambrose212@gmail.com>
2024-07-05 20:07:16 +0300

    first commit

---
+++ src/main/java/AuthGuard/SecureAuthTracker/SecureAuthTrackerApplication.java
@@ -0,0 +1,13 @@
+package AuthGuard.SecureAuthTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureAuthTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureAuthTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/AuthGuard/SecureAuthTracker/SecureAuthTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package AuthGuard.SecureAuthTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureAuthTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
