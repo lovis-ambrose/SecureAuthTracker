ebf8d5e3b1141a4071397b83437094858670bd17
lovis-ambrose <lovisambrose212@gmail.com>
2024-07-15 12:41:16 +0300

    code clean up

--- src/main/java/AuthGuard/SecureAuthTracker/resounces/UserResounce.java
+++ src/main/java/AuthGuard/SecureAuthTracker/resounces/UserResounce.java
@@ -1,13 +1,23 @@
 package AuthGuard.SecureAuthTracker.resounces;
 
+import AuthGuard.SecureAuthTracker.domains.HttpResponse;
+import AuthGuard.SecureAuthTracker.domains.User;
+import AuthGuard.SecureAuthTracker.dto.UserDTO;
 import AuthGuard.SecureAuthTracker.services.UserService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import java.net.http.HttpResponse;
+import java.net.URI;
+
+import static java.time.LocalDateTime.now;
+import static java.util.Map.of;
+import static org.springframework.http.HttpStatus.CREATED;
 
 @RestController
 @RequestMapping(path = "/user")
@@ -15,5 +25,20 @@ import java.net.http.HttpResponse;
 public class UserResounce {
     private final UserService userService;
     @PostMapping("/register")
-    public ResponseEntity<HttpResponse> registerUser
+    public ResponseEntity<HttpResponse> saveUser(@RequestBody @Valid User user) {
+        UserDTO userDTO = userService.createUser(user);
+        return ResponseEntity.created(getUri()).body(
+                HttpResponse.builder()
+                        .timeStamp(now().toString())
+                        .data(of("user", userDTO))
+                        .message("User created")
+                        .status(CREATED)
+                        .statusCode(CREATED.value())
+                        .build()
+        );
+    }
+
+    private URI getUri() {
+        return URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/user/get/<userId>").toUriString());
+    }
 }
